87480a90bfe1127ce93924e4ef9cfff33b27e4fe
Lidia <lidia.rojek@gmail.com>
2021-04-27 23:37:14 +0200

    poprawki_2

--- game.js
+++ game.js
@@ -1,13 +1,21 @@
 const square = document.querySelector('.square');
+const container = document.querySelector('.container');
 
 const interval = setInterval(() => {
-    const body = document.body;
-    const squareSide = square.offsetHeight;
-    const windowWidth = body.clientWidth;
-    const windowHeight = body.clientHeight;
-    const top = Math.floor((Math.random() * windowHeight) + squareSide);
-    const right = Math.floor((Math.random() * windowWidth) + squareSide);
-    square.style.top = `calc(${top}px - ${squareSide}px)`;
-    square.style.right = `calc(${right}px - ${squareSide}px)`;
-    console.log(top, right);
+    setRandomSquarePosition(container, square);
 }, 1000);
+
+function setRandomSquarePosition(container, square) {
+    const squareSide = square.offsetHeight;
+    const height = container.clientHeight;
+    const width = container.clientWidth;
+    const top = getRandom(squareSide, height);
+    const right = getRandom(squareSide, width);
+    square.style.top = `${top-squareSide}px`; 
+    square.style.right = `${right-squareSide}px`;
+    console.log(top, right);
+}
+
+function getRandom(from, to) {
+    return Math.floor(Math.random() * (to - from + 1) + from);
+}
