16b22f48616c3a485b5ede668b4e499e85ae9efb
Lidia <lidia.rojek@gmail.com>
2021-06-09 13:51:44 +0200

    firebase - first try

--- game.js
+++ game.js
@@ -1,12 +1,30 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyCy48otM_L7syhkwg3hXgVyp830i95P69o",
+    authDomain: "game-73643.firebaseapp.com",
+    databaseURL: "https://game-73643-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "game-73643",
+    storageBucket: "game-73643.appspot.com",
+    messagingSenderId: "100100717887",
+    appId: "1:100100717887:web:3f973da66b237cf45c678a"
+  };
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+var database = firebase.database();
+
 const minDistance = 200;
 
-
 let counter = 0;
-
-
+const updatePlayerResult = () => {
+    firebase.database().ref("players").push().set({
+        "name": player,
+        "game": gameName,
+        "result": counter,
+    });
+}
 const onSquareClick = (event) => {
     event.target.querySelector(".count").innerText = ++counter;
-
+    updatePlayerResult();
     moveSquareAfterTimeout();
 }
 
@@ -29,7 +47,7 @@ function moveSquareAfterTimeout() {
 
     timeout = setTimeout(() => {
         moveSquareAfterTimeout()
-    }, 200);
+    }, 2000);
 }
 
 moveSquareAfterTimeout();
@@ -66,7 +84,6 @@ function getNextRandomPosition(container, square) {
 
 }
 
-
 function setSquarePosition({top, left, square}) {
     square.style.top = `${top}px`;
     square.style.left = `${left}px`;
@@ -80,7 +97,6 @@ function getRandomWithBindSpot(from, to, blindFrom, blindTo) {
         random = random + (blindTo - blindFrom);
     }
 
-
     if (random > to) {
         return to;
     }
@@ -92,3 +108,24 @@ function getRandomWithBindSpot(from, to, blindFrom, blindTo) {
     return random;
 
 }
+
+let player = prompt("Type your name");
+let gameName = "1st round";
+
+const setPlayer = () => {
+   
+let players = firebase.database().ref("players").push().set({
+    "name": player,
+    "game": gameName,
+    "result": 5,
+});
+
+var newResult = firebase.database().ref().child('results').push().key;
+
+var updates = {};
+updates['/results/' + newResult] = players;
+return firebase.database().ref().update(updates);
+}
+
+setPlayer();
+
